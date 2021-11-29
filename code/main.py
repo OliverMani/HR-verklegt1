908e19e3205865e836660d173a0398a959ca94a0
Selma Lind Davíðsdóttir <89972166+SelmaLD@users.noreply.github.com>
2021-11-29 14:54:28 +0000

    byrjaði að gera loggin til að finna stöðu starfsmanna

--- code/main.py
+++ code/main.py
@@ -1,11 +1,17 @@
 def main():
     # user = input("Nafn: ")
-    user = "Jan Jacobsen"
+    user = "Nanna Daema"
     user_pos  = type_of_user(user)
+    print(user_pos)
 
 def type_of_user(user):
-    open_file = open("code/staff.csv", "r", encoding="UTF-8")
+    open_file = open("csv_files/staff.csv", "r", encoding="UTF-8")
     for line in open_file:
-        print(line)
+        line = line.split(",")
+        if user in line:
+           return (line[7])
+        
+
+            
 if __name__ == "__main__":
     main()
