bce256cd2c17b9f65e410d4637a463e5d1a4bcd6
Ivar Smari Bjørnsson <Ivarb21@ru.is>
2021-11-30 10:26:01 +0000

    comments í LL og modules

--- code/logiclayer/yfirmadur.py
+++ code/logiclayer/yfirmadur.py
@@ -1,9 +1,10 @@
 class YfirmadurLL:
+    """Höndlar allt varðandi Yfirmenn"""
     def __init__(self, starfsmadur):
         pass
 
-    def __init__(self):
-        pass
 
     def finna_starfsmann(self, audkenni):
+        """leitar af starfsmanni í starfsmannaskránni frá data layer
+        og skilar upplýsingum um starfsmanninn"""
         pass
--- code/modules/verkskyrsla.py
+++ code/modules/verkskyrsla.py
@@ -1,4 +1,5 @@
 class VerkSkyrsla:
+    """klasi sem tekur inn allar upplýsingar varðandi Verkskýrslum"""
     def __init__(self, titill, starfsmadur, lysing, dags, lokid, samtykkt):
         self.titill = titill
         self.starfsmadur = starfsmadur
