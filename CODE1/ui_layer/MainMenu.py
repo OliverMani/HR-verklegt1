4779e4016644445f45b5aee066abd50ac75b1ed1
OliverMani <oliverhilmars@gmail.com>
2021-11-30 15:57:21 +0000

    lagaði aðeins, tók út options og lét það vera það sama og menu í main menu

--- CODE1/ui_layer/MainMenu.py
+++ CODE1/ui_layer/MainMenu.py
@@ -7,10 +7,15 @@ class Main_menu:
         self.menu = """
 (P)rófíll    (V)erkefni    (F)asteignir    (S)tarfsmenn \t <(T)il baka>   <(Q) Hætta>
 -------------------------------------------------------------------------------------------"""
+        # Hér er listi af instances af skjám, gerum þetta til að þurfa ekki
+        # að gera ný og ný instances í hvert skipti sem við flökkum á milli skjáa
+        #self.screens = [EmployeeListScreen(), PropertyListScreen()]
+        #self.current_screen = 0
 
     def menubar(self):
         print(self.menu)
-        selected = input("Slá inn aðgerð: ").lower()
+        selected = input("\nSlá inn aðgerð: ").lower()
+        #self.screens[self.current_screen].render()
         while selected != "q":
             print(self.menu)
             if selected == "p":
@@ -18,9 +23,13 @@ class Main_menu:
             elif selected == "v":
                 print("Verkefni")
             elif selected == "f":
+                #self.current_screen = 1
+                #self.render()
                 screen = PropertyListScreen()
                 screen.render()
             elif selected == "s":
+                #self.current_screen = 0
+                #self.menubar()
                 screen = EmployeeListScreen()
                 screen.render()
             elif selected == "t":
@@ -29,4 +38,4 @@ class Main_menu:
                 return
             else:
                 print("Aðgerð ekki til ")
-            selected = input("Slá inn aðgerð: ").lower()
+            selected = input("\nSlá inn aðgerð: ").lower()
