91d7f2779f135327546906fabc381ec73dbec9c1
OliverMani <oliverhilmars@gmail.com>
2021-11-30 13:05:30 +0000

    Lagaði merge

--- code/modules/afangastadur.py
+++ code/modules/afangastadur.py
@@ -1,11 +1,7 @@
 class Afangastadur:
-<<<<<<< HEAD
+    """tekur inn allar upplýsingar varðandi áfangastaði"""
     def __init__(self, id, land, flugvollur, simanumer, opnunartimi, yfirmadur, fasteignir):
         self.id = id
-=======
-    """tekur inn allar upplýsingar varðandi áfangastaði"""
-    def __init__(self, land, flugvollur, simanumer, opnunartimi, yfirmadur, fasteignir):
->>>>>>> 94d9d91b3907807ec14ca7eb5bea020bcca154b8
         self.land = land
         self.flugvollur = flugvollur
         self.simanumer = simanumer
--- code/modules/fasteign.py
+++ code/modules/fasteign.py
@@ -1,16 +1,10 @@
 class Fasteign:
-<<<<<<< HEAD
     def __init__(self, id, stadur, heimilisfang, fm, herbergi, tegund, fasteignanumer, active):
         self.id = id
         self.heimilisfang = heimilisfang
         self.fm = fm
         self.herbergi = herbergi
         self.tegund = tegund
-=======
-    """Fasteignarklasi sem tekur inn allar nauðsynlegu upplýsingar fyrir fasteignir"""
-    def __init__(self, audkenni, fasteignarnumer, astand, adstada, afangastadur, verkbeidnir):
-        self.audkenni = audkenni
->>>>>>> 94d9d91b3907807ec14ca7eb5bea020bcca154b8
         self.fasteignarnumer = fasteignarnumer
         self.active = active
 
