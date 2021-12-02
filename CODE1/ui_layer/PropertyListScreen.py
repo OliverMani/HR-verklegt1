0d3d741b0227ad93063ee5899188a182d3b1bfaa
Selma Lind Davíðsdóttir <89972166+SelmaLD@users.noreply.github.com>
2021-12-02 09:29:27 +0000

    bætti við leit í fasteignum

--- CODE1/ui_layer/PropertyListScreen.py
+++ CODE1/ui_layer/PropertyListScreen.py
@@ -10,9 +10,39 @@ class PropertyListScreen:
         print("Fasteignir\n")
         print('\n'.join([(x.id + '. ' + x.heimilisfang) for x in properties]))
 
-    def search_in_list(self):
-        pass
+    def search_in_list(self, word):
+        found = False
+        properties = self.llapi.get_property_list()
+        for property in properties:
+            if property.id == word:
+                self.print_result(property)
+            elif property.staður == word:
+                self.print_result(property)
+            elif property.heimilisfang == word:
+                self.print_result(property)
+            elif property.fm == word:
+                self.print_result(property)
+            elif property.herbergi == word:
+                self.print_result(property)
+            elif property.tegund == word:
+                self.print_result(property)
+            elif property.fasteignanúmer == word:
+                self.print_result(property)
+            else:
+                print("Ekkert fannst")
+                
+    def print_result(self, property):
+
+        print("\n",property.heimilisfang)
+        print(property.staður)
+        print("fasteignarnúmer", property.fasteignanúmer)
+        print()
+
+
+
+
+
 
     #filter
-    def sort_list(self):
+    def sort_list(self, word):
         pass
