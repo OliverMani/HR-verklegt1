6a096d6389b758b26aca9fd722441eb08310e607
OliverMani <oliverhilmars@gmail.com>
2021-12-02 10:33:56 +0000

    Setti inn ID  inn í skýrslu módelið

--- CODE1/Model/WorkReport.py
+++ CODE1/Model/WorkReport.py
@@ -1,6 +1,7 @@
 class WorkReport:
     """klasi sem tekur inn allar upplýsingar varðandi Verkskýrslum"""
-    def __init__(self, titill, starfsmadur, lysing, dags, lokid, samtykkt):
+    def __init__(self, id, titill, starfsmadur, lysing, dags, lokid, samtykkt):
+        self.id = id
         self.titill = titill
         self.starfsmadur = starfsmadur
         self.lysing = lysing
