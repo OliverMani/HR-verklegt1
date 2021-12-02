15081f72f1f8b08c9e2efd78854b32325012dc6c
Ivar Smari Bjørnsson <Ivarb21@ru.is>
2021-12-02 10:04:13 +0000

    til að leyfa mér að pulla

--- CODE1/ui_layer/PropertyListScreen.py
+++ CODE1/ui_layer/PropertyListScreen.py
@@ -10,6 +10,42 @@ class PropertyListScreen:
         print("Fasteignir\n")
         print('\n'.join([(x.id + '. ' + x.heimilisfang) for x in properties]))
 
+    def search_in_list(self, word):
+        found = False
+        properties = self.llapi.get_property_list()
+        for property in properties:
+            if property.id == word:
+                self.print_result(property)
+            elif property.staður == word:
+                self.print_result(property)
+            elif property.heimilisfang == word:
+                self.print_result(property)
+            elif property.fm == word:
+                self.print_result(property)
+            elif property.herbergi == word:
+                self.print_result(property)
+            elif property.tegund == word:
+                self.print_result(property)
+            elif property.fasteignanúmer == word:
+                self.print_result(property)
+            else:
+                print("Ekkert fannst")
+                
+    def print_result(self, property):
+
+        print("\n",property.heimilisfang)
+        print(property.staður)
+        print("fasteignarnúmer", property.fasteignanúmer)
+        print()
+
+
+
+
+
+
+    #filter
+    def sort_list(self, word):
+        pass
     # þarf að klára....
     def search_in_list(self, search):
         found = False
