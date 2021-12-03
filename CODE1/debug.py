92b1b0319b62b8d6b73a7f55b97f4de70456d0e4
Sunnevas <sunnevas21@ru.is>
2021-12-03 15:22:10 +0000

    debug

--- CODE1/debug.py
+++ CODE1/debug.py
@@ -1,6 +1,6 @@
 from ui_layer.EmployeeListScreen import EmployeeListScreen
 from ui_layer.PropertyListScreen import PropertyListScreen
-from ui_layer.WorkReport import WorkReport
+from ui_layer.WorkReport import WorkReportlistscreen
 from ui_layer.WorkRequest import WorkRequest
 # a = EmployeeListScreen()
 # a.create_new_employee()
