ac7c48d253c3c2e8bf2a556c79da711cb15465d4
OliverMani <oliverhilmars@gmail.com>
2021-12-06 10:36:32 +0000

    Setti aftur input fyrir nafn

--- CODE1/main.py
+++ CODE1/main.py
@@ -7,17 +7,13 @@ from ui_layer.LoginScreen import Login
 
 if __name__ == "__main__":
     """ Ef notandinn er til þá opnar forritið annars er aftur beðið um innskráningu"""
-    # user_input = input("Nafn: ")
-    user_input = "Jan Jacobsen"
+    user_input = input("Nafn: ")
+    #user_input = "Jan Jacobsen"
     user = Login(user_input).login()
-    
+
     while user == None:
         user_input = input("Nafn: ")
         user = Login(user_input).login()
-    
+
     main_menu = Main_menu(user)
     main_menu.menubar()
-
-
-    
-    
