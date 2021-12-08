101085056e360a7da041b19a5a4e9b97430787ae
Selma Lind Davíðsdóttir <89972166+SelmaLD@users.noreply.github.com>
2021-12-08 11:06:27 +0000

    samþykkja verkskýrslur kemur bara hjá yfirmönnum og smá lagfæringar

--- CODE1/debug.py
+++ CODE1/debug.py
@@ -1,9 +1,13 @@
 from LogicLayer.LLAPI import LLAPI
+from Storagelayer.SLAPI import Slapi
+
+work_report_list = Slapi().get_work_report_list()
+property_list = Slapi().get_property_list()
 
 
-prop_list = (LLAPI().get_property_by_id("2").heimilisfang)
+print(LLAPI().get_work_reports_by_property("3"))
 
-print(prop_list)
-
+print(work_report_list)
+print(property_list)
 
     
