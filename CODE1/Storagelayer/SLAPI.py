b6a65e7c499a2f9a376f0148f89c8c398dd09a7c
Ivar Smari Bjørnsson <Ivarb21@ru.is>
2021-12-08 12:06:03 +0000

    Gerði LLAPI og SLAPI sexy ;)

--- CODE1/Storagelayer/SLAPI.py
+++ CODE1/Storagelayer/SLAPI.py
@@ -13,53 +13,60 @@ class Slapi:
         self.workrepSL = WorkReportData()
         self.destSL = DestinationData()
 
+#----------------------- Get föll ------------------------------#
+
     def get_destination_list(self):
+        ''' skilar destination list '''
         return self.destSL.open_file()
 
     def get_employee_list(self):
-        '''skilar employee list'''
+        ''' skilar employee list '''
         return self.empSL.open_file()
 
     def get_work_request_list(self):
-        '''skilar work request list'''
+        ''' skilar work request list '''
         return self.wrSL.open_file()
 
     def get_property_list(self):
-        '''skilar property list'''
+        ''' skilar property list '''
         return self.proSL.open_file()
 
     def get_work_report_list(self):
-        '''skilar work report list'''
+        ''' skilar work report list '''
         return self.workrepSL.open_file()
 
-#-------------------
+#----------------------- Create föll ------------------------------#
 
     def create_new_employee(self,emp):
-        '''Býr til nýjan starfsmann'''
+        ''' Býr til nýjan starfsmann '''
         return self.empSL.create_new_employee(emp)
 
     def create_new_property(self, prop):
-        """ Skráir inn nýjar fasteignir"""
+        """ Skráir inn nýjar fasteignir """
         return self.proSL.create_new_property(prop)
 
     def create_new_work_report(self,report):
-        '''Setur inn nýja verkskýrslu'''
+        ''' Setur inn nýja verkskýrslu '''
         return self.workrepSL.create_new_work_report(report)
 
     def create_new_work_request(self, req):
-        """ Setur inn nýja verkbeiðni"""
+        """ Setur inn nýja verkbeiðni """
         return self.wrSL.create_new_work_request(req)
 
 #----------------------- update föll ------------------------------#
 
     def update_employee(self, employee):
+        ''' uppfærir employee csv skránna '''
         return self.empSL.update(employee)
 
     def update_property(self, property):
+        ''' uppfærir upplýsingar sem notandi breytir um fasteign í property csv skránni '''
         return self.proSL.update(property)
 
     def update_work_report(self, work_report):
+        ''' uppfærir upplýsingar varðandi work report sem notandi vildi uppfæra í work report csv skránni '''
         return self.workrepSL.update(work_report)
 
     def update_work_request(self, work_request):
+        ''' uppfærir upplýsingar um work request sem notandi vill uppfæra '''
         return self.wrSL.update(work_request)
