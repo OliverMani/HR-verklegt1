c17cd690b66011d162f1e20474d7840738f4038d
olafur95 <olafurbaering@gmail.com>
2021-12-09 18:41:41 +0000

    work request breyta
    work request breyta takki

--- CODE1/ui_layer/WorkRequest.py
+++ CODE1/ui_layer/WorkRequest.py
@@ -87,6 +87,7 @@ class WorkRequestListScreen:
             print("(A) Sjá allar skráðar verkbeiðnir")
             print("(C) búa til nýja verkebiðni fyrir fasteign")
             print("ID + (CVS) búa til nýja verkebiðni fyrir fasteign")
+            print("(B) Breyta verkbeiðni fyrir fasteign")
 
             # print("(undefined) Loka verkefni ")
             # print("(undefined) Breyta verkbeiðni fyrir fasteign")
