2ddf1941a16be55288adbfd03fb6f36ddb114951
OliverMani <oliverhilmars@gmail.com>
2021-12-09 15:01:29 +0000

    Lagaði villur í kringum verkefnin

--- CODE1/Model/WorkReport.py
+++ CODE1/Model/WorkReport.py
@@ -1,9 +1,9 @@
 class WorkReport:
     """klasi sem tekur inn allar upplýsingar varðandi Verkskýrslum"""
-    def __init__(self, id, titill, verbeidniID, starfsmadurID, verktaki, lysing, dags, timi, kostnadur, heimilisfang, lokid, samthykkt):
+    def __init__(self, id, titill, verkbeidniID, starfsmadurID, verktaki, lysing, dags, timi, kostnadur, heimilisfang, lokid, samthykkt):
         self.id = id
         self.titill = titill
-        self.verbeidniID = verbeidniID
+        self.verkbeidniID = verkbeidniID
         self.starfsmadurID = starfsmadurID
         self.verktaki = verktaki
         self.lysing = lysing
@@ -13,7 +13,6 @@ class WorkReport:
         self.heimilisfang = heimilisfang
         self.lokid = lokid
         self.samthykkt = samthykkt
-    
+
     def __str__(self):
         return f"id:{self.id}, titill:{self.titill}, verkbeidniID:{self.verbeidniID}, starfsmadurID: {self.starfsmadurID}, verktaki: {self.verktaki}, lysing: {self.lysing}, dags: {self.dags}, timi: {self.timi}, kostnadur: {self.kostnadur}, heimilisfang: {self.heimilisfang}, lokid: {self.lokid}, samtykkt: {self.samtykkt}"
-
