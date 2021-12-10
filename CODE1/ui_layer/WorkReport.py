2c0143d62ad8b9e79888440de76c444ee1b69ca9
Selma Lind Davíðsdóttir <89972166+SelmaLD@users.noreply.github.com>
2021-12-10 13:44:15 +0000

    Ekki hægt að samþykkja samþykkta skýrslu

--- CODE1/ui_layer/WorkReport.py
+++ CODE1/ui_layer/WorkReport.py
@@ -63,7 +63,9 @@ Staðsetning:
     {work_report.heimilisfang}
 """
         print(work_report_str)
-        if self.llapi.get_current_user().stada.lower() == ("yfirmaður" or "eigandi"):
+        if work_report.samthykkt == "true":
+            print("Skýrsla hefur verið samþykkt!")
+        elif self.llapi.get_current_user().stada.lower() == ("yfirmaður" or "eigandi"):
             accept = input("Viltu samþykkja þessa verkskýrslu? (j/n): ")
             if accept.lower() == 'j':
                 self.llapi.accept_work_report_by_id(work_report.id) #Keyra ferli sem samþykkir skýrslu
